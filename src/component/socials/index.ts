edb548af5c6dde06e67dcd5674173e66a6a6ff8a
dumsterB <dumsterfordevelop@gmail.com>
2023-06-10 12:36:13 +0500

    update and refactor

--- src/component/socials/index.ts
+++ src/component/socials/index.ts
@@ -1,43 +1,48 @@
 import { Socials } from "../../types/index";
 import {
-  instagram,
-  telegram,
-  message,
-  support,
-  twitter,
-  facebook,
-} from "../../utils/icons";
-import "./index.css";
-let components  = {
     instagram,
     telegram,
     message,
     support,
     twitter,
     facebook,
+} from "../../utils/icons";
+import "./index.css";
+
+interface ComponentMap {
+    [key: string]: string;
 }
 
+const components: ComponentMap = {
+    instagram,
+    telegram,
+    message,
+    support,
+    twitter,
+    facebook,
+};
+
 export default function SocialMedias(params: Socials[]): string {
-    const socials:HTMLDivElement = document.createElement('div')
-    if(params && params.length){
-        for(let i = 0; i < params.length; i ++){
-            const social_item  =document.createElement('div')
-            social_item.classList.add('social-item')
-            const social_icon = document.createElement('div')
-            social_icon.classList.add('social-icon')
-            const text  = document.createElement('p')
-            text.classList.add('social-name')
-            text.textContent = params[i].type
-            social_item.appendChild(social_icon)
-            social_item.appendChild(text)
-            social_icon.innerHTML = components[params[i].type]
-            socials.appendChild(social_item)
+    const socials: HTMLDivElement = document.createElement("div");
+    if (params && params.length) {
+        for (let i = 0; i < params.length; i++) {
+            const social_item = document.createElement("div");
+            social_item.classList.add("social-item");
+            const social_icon = document.createElement("div");
+            social_icon.classList.add("social-icon");
+            const text = document.createElement("p");
+            text.classList.add("social-name");
+            text.textContent = params[i].type;
+            social_item.appendChild(social_icon);
+            social_item.appendChild(text);
+            social_icon.innerHTML = components[params[i].type];
+            socials.appendChild(social_item);
         }
     }
 
-    let content
-    if(params && params.length) {
-  content = `
+    let content;
+    if (params && params.length) {
+        content = `
     <div>
       <div class="max-w-2xl">
         <div class="grid grid-cols-5 gap-1 socials flex mt-2 border-b-2">
@@ -47,10 +52,9 @@ export default function SocialMedias(params: Socials[]): string {
       </div>
     </div>
   `;
-    }else{
-        return `<div></div>`
+    } else {
+        return `<div></div>`;
     }
 
-
-  return content;
+    return content;
 }
--- src/services/postGPT.ts
+++ src/services/postGPT.ts
@@ -1,4 +1,4 @@
-import {io, Socket} from "socket.io-client";
+//import {io, Socket} from "socket.io-client";
 
 // export default function sendMessageToChatGPT(message: string): Promise<string> {
 //     return new Promise<string>((resolve, reject) => {
