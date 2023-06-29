33bdf25bd3f4820713c52698fcc87a72c9d82435
Fackster <fackster.ad@gmail.com>
2023-06-29 18:13:39 +0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { MessangerConfig } from "./types/index.ts";
 
 const config: MessangerConfig = {
   holder: "messenger",
-  socials: [
+  
+socials: [
   { link: "https://instagram.com/name_of_account", type: "instagram" },
   { link: "https://t.me/name_of_account", type: "telegram" },
   { link: "https://facebook.com/name_of_account", type: "facebook" },
   { link: "https://twitter.com/name_of_account", type: "twitter" }
 ],
+  
   name: "Dumster Ai",
   header_background: `url(https://code-jvs1.jivosite.com/images/pattern/1.svg)`,
   token: import.meta.env.VITE_TOKEN,
